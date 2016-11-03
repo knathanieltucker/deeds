0bbea6fb1e982e8a6b0659c91c19195d0ea7ec60
K. Nathaniel Tucker <k.nathaniel.tucker@gmail.com>
2016-11-03 09:09:30 -0700

    added packaged.json
    Added in the real package.json and a test case that passes

---
+++ client/main.test.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+import { chai } from 'meteor/practicalmeteor:chai';
+
+describe('default', function () {
+  it('passes', function () {
+    chai.assert.equal(true, true);
+  });
+});
