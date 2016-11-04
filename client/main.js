c028bc27077849b9c84d0a5160f9bebd6bc75940
K. Nathaniel Tucker <k.nathaniel.tucker@gmail.com>
2016-11-03 17:31:09 -0700

    Commits: test commit

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+// test
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
